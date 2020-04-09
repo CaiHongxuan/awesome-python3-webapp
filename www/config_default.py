8154895eda9f950c1c02a654c7cd6fa22d44c1fd
1203673288@qq.com <1203673288@qq.com>
2020-04-09 22:11:24 +0800

    add mvc and config file

---
+++ www/config_default.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Hongxuan'
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'www-data',
+        'password': 'www-data',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret': 'Afg$suy'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+Override configurations.
+"""
+
+__author__ = 'Hongxuan'
+
+
+configs = {
+    'db': {
+        'host': '127.0.0.1'
+    }
+}
